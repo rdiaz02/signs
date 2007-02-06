24dd1082f2b987e0c58ea24f3fe0441d29d4dd71
ramon diaz-uriarte <ramon@xantusia>
2007-02-06 16:45:44 +0100

    more web benchmarks

--- Benchmarks/wrapWebBenchmarks1.py
+++ Benchmarks/wrapWebBenchmarks1.py
@@ -42,12 +42,43 @@ def writeFile(testout, name):
     fout.close()
     
 
-dlbcl = launchAll('dlbcl', NUM_USERS)
-writeFile(dlbcl, 'dlbcl.web.bnchmk.' + outappend + 'txt')
+# dlbcl = launchAll('dlbcl', NUM_USERS)
+# writeFile(dlbcl, 'dlbcl.web.bnchmk.' + outappend + 'txt')
 
 
-breast = launchAll('breast', NUM_USERS)
-writeFile(breast, 'breast.web.bnchmk.' + outappend + 'txt')
-
+# breast = launchAll('breast', NUM_USERS)
+# writeFile(breast, 'breast.web.bnchmk.' + outappend + 'txt')
 
 ## 2362.9028670799999, 2368.0365898599998: dlbcl with 1 sample
+
+## OK, this is a very ugly thing, but I was getting random crashes
+## that lost all work. Now I do this from an interactive python shell.
+
+breast_1_1 = launchUTests('breast', 1)
+breast_1_2 = launchUTests('breast', 1)
+breast_1_3 = launchUTests('breast', 1)
+breast_1_4 = launchUTests('breast', 1)
+breast_1_5 = launchUTests('breast', 1)
+breast_5_1 = launchUTests('breast', 5)
+breast_10_1 = launchUTests('breast', 10)
+breast_20_1 = launchUTests('breast', 20)
+
+breast = []
+breast = breast_1_1 + breast_1_2 + breast_1_3 + breast_1_4 + breast_1_5 +\
+         breast_5_1 + breast_10_1 + breast_20_1
+
+writeFile(breast, 'breast.web.tests.txt')
+
+dlbcl_1_1 = launchUTests('dlbcl', 1)
+dlbcl_1_2 = launchUTests('dlbcl', 1)
+dlbcl_1_3 = launchUTests('dlbcl', 1)
+dlbcl_1_4 = launchUTests('dlbcl', 1)
+dlbcl_1_5 = launchUTests('dlbcl', 1)
+dlbcl_5_1 = launchUTests('dlbcl', 5)
+dlbcl_10_1 = launchUTests('dlbcl', 10)
+dlbcl_20_1 = launchUTests('dlbcl', 20)
+
+dlbcl = []
+dlbcl = dlbcl_1_1 + dlbcl_1_2 + dlbcl_1_3 + dlbcl_1_4 + dlbcl_1_5 +\
+         dlbcl_5_1 + dlbcl_10_1 + dlbcl_20_1
+
