74c32e21ebd104cd6230b73fc17c6bef62c38ec1
RDU <rdiaz@ligarto.org>
2007-02-26 15:00:02 +0100

    figures paper

--- Benchmarks/plot.web.benchmarks.R
+++ Benchmarks/plot.web.benchmarks.R
@@ -7,22 +7,10 @@ rr <- function(x) rep(x, x)
 
 breast.results <- scan("breast.web.tests.txt", sep = "\t",
                        what = double(0), 40)
+dlbcl.results <- scan("dlbcl.web.tests.txt", sep = "\t",
+                       what = double(0), 40)
+
 nusers <- c(rep(1, 5), rr(5), rr(10), rr(20))
-
-            
-
-
-
-dlbcl.results <- NULL
-dlbcl.results <- c(dlbcl.results,
-                    scan("dlbcl.web.bnchmk.1.1.1.txt", sep = "\t", what = double(0), nmax = 3),
-                    scan("dlbcl.web.bnchmk.A.1.20txt", sep = "\t", what = double(0), nmax = 38),
-                    scan("dlbcl.web.bnchmk.B.1.20txt", sep = "\t", what = double(0), nmax = 38),
-                    scan("dlbcl.web.bnchmk.F.1.20txt", sep = "\t", what = double(0), nmax = 38))
-
-nusers <- c(rep(1, 3), rep(c(rr(1), rr(2), rr(5), rr(10), rr(20)), 3))
-
-
 
 
 postscript(file = "bench.web.eps", height = 8, width = 12,
@@ -40,3 +28,5 @@ boxplot(breast.results ~ nusers, xlab = "Number of simultaneous users",
 boxplot(dlbcl.results ~ nusers, xlab = "Number of simultaneous users",
         ylab = "User wall time (seconds)",
         main = "DLBCL data set (160 arrays x 7399 genes)")
+
+dev.off()
